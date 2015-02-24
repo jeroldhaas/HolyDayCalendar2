cee113bffbf9fc8ef1384d98f14b9186dd0a9361
Jerold Haas <jeroldhaas@gmail.com>
2015-02-23 20:42:54 -0500

    ordered, commented, and configured feasts

--- HolyDayCalendarFSharpCLI/HolyDays.fs
+++ HolyDayCalendarFSharpCLI/HolyDays.fs
@@ -73,6 +73,7 @@ module HolyDay =
         // rules, so need to be expanded rules
         // http://afaithfulversion.org/leviticus-23/
         [
+            // 14th day of the 1st month
             {
                 Name    = "Passover";
                 Month   = Some 1;
@@ -80,6 +81,14 @@ module HolyDay =
                 Weekday = None;
                 Length  = 1;
             };
+            { // TODO: discovery - Numbers 9:6-13
+                Name    = "2nd Passover";
+                Month   = Some 2;
+                Day     = Some 14;
+                Weekday = None;
+                Length  = 1;
+            };
+            // succeeding day after passover
             {
                 Name    = "Feast of Unleavened Bread";
                 Month   = Some 1;
@@ -87,6 +96,7 @@ module HolyDay =
                 Weekday = None;
                 Length  =  7;
             };
+            // 49th day after Sabbath following Unleavened Bread
             {
                 Name    = "Pentecost";
                 Month   = None;
@@ -94,39 +104,35 @@ module HolyDay =
                 Weekday = Some DayOfWeek.Saturday;
                 Length  = 1;
             };
+            // 1st day of 7th month
             {
                 Name    = "Feast of Trumpets";
-                Month   = Some 2;
-                Day     = Some 2;
+                Month   = Some 7;
+                Day     = Some 1;
+                Weekday = None;
+                Length  = 1;
+            };
+            // 10th day of 7th month is Atonement
+            {
+                Name    = "Day of Atonement";
+                Month   = Some 7;
+                Day     = Some 10;
+                Weekday = None;
+                Length  = 1;
+            };
+            // 15th day of 7th month starts the week-long feast
+            {
+                Name    = "Feast of Tabernacles";
+                Month   = Some 7;
+                Day     = Some 15;
                 Weekday = None;
                 Length  = 7;
             };
-            { // TODO: discovery
-                Name    = "2nd Passover";
-                Month   = Some 3;
-                Day     = Some 3;
-                Weekday = None;
-                Length  = 1;
-            };
-            {
-                Name    = "Day of Atonement";
-                Month   = Some 5;
-                Day     = Some 5;
-                Weekday = None;
-                Length  = 1;
-            };
-            {
-                Name    = "Feast of Tabernacles";
-                Month   = Some 5;
-                Day     = Some 5;
-                Weekday = None;
-                Length  = 1;
-            };
             {
                 Name    = "Last Great Day";
-                Month   = Some 5;
-                Day     = Some 5;
-                Weekday = None;
+                Month   = Some 7;
+                Day     = Some 22;
+                Weekday = Some DayOfWeek.Saturday;
                 Length  = 1;
             };
         ]
